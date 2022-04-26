51e867d354487c2617e9949833b9935f224df995
Magdalena <krystynapawlowicz@wp.pl>
2022-04-26 18:09:31 +0200

    Add BankApp class & set for parameters and get and set for name param

---
+++ src/main/java/BankApp.java
@@ -0,0 +1,13 @@
+public class BankApp {
+    private String name;
+    private String surname;
+    private String accNumber;
+    private long balance;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+}
